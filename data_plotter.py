199bf4dfd700b727d420a9b76c8c198c3558d725
Fraser Watt <94307749+fraserw4tt@users.noreply.github.com>
2021-12-04 21:06:07 +0000

    update to working version

--- data_plotter.py
+++ data_plotter.py
@@ -6,31 +6,31 @@ from matplotlib import pyplot as plt
 
 def labels():
     labels = []
-    
+
     ext = input('What is the title of your graph? ')
     labels.append(ext)
-    
+
     ext = input('What would you like to label your x axis? ')
     labels.append(ext)
-    
+
     ext = input('What would you like to label your y axis? ')
     labels.append(ext)
-    
+
     plt.title(labels[0])
     plt.xlabel(labels[1])
     plt.ylabel(labels[2])
 
-    
+
 def x_values():                                                 
     '''
     Asks user for sample size before iterating over input command by user defined range.
     while True tests for ability to convert input to float to force accepted input.
     '''
+    global x_sample
+    x_sample = []                                                
+    print('Please enter your x values.')
     
-    x_sample = []                                               
-    x_pop = int(input('Please enter the x axis sample size: ')) 
-    
-    for i in range(0, x_pop):
+    for i in range(0, pop):
         while True:
             ext = input()
             ## y_sample.append(ext)  
@@ -43,20 +43,20 @@ def x_values():
                 continue
             else:
                 ext = float(ext)
-                y_sample.append(ext)
-                break)                                    
-        
-        
+                x_sample.append(ext)
+                break                                  
+
+
 def y_values():  
     '''
     Asks user for sample size before iterating over input command by user defined range.
     while True tests for ability to convert input to float to force accepted input.
     '''
-    
-    y_sample = []                                               
-    y_pop = int(input('Please enter the y axis sample size: ')) 
-    
-    for i in range(0, y_pop):
+    global y_sample
+    y_sample = [] 
+    print('Please enter your y values.')
+
+    for i in range(0, pop):
         while True:
             ext = input()
             ## y_sample.append(ext)  
@@ -71,14 +71,14 @@ def y_values():
                 ext = float(ext)
                 y_sample.append(ext)
                 break
-        
-        
+
+
 def x_scale():
     '''
     Uses while True to test for accepted input and prevent errors.
     Plot scales can be changed using log or linear condition in plt.xscale()
     '''
-    
+
     while True:
         x_scale = input('What scale would you like to make the x axis? linear / log ')
         if x_scale not in ['linear', 'log']:
@@ -86,16 +86,16 @@ def x_scale():
             continue
         else:
             break
-    
+
     plt.xscale(x_scale)
-          
-        
+
+
 def y_scale():
     '''
     Uses while True to test for accepted input and prevent errors.
     Plot scales can be changed using log or linear condition in plt.xscale()
     '''
-    
+
     while True:
         y_scale = input('What scale would you like to make the y axis? linear / log ')
         if y_scale not in ['linear', 'log']:
@@ -103,14 +103,17 @@ def y_scale():
             continue
         else:
             break
-    
+
     plt.yscale(y_scale)
-        
+
 ################################
 ## Main Body
 ################################
 
 labels()
+
+pop = int(input('Please enter the sample size: '))
+
 x_values()
 y_values()
 
